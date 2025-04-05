c84725bc0d2bb31d14754b04f562121670998757
Toni Lozano <aj.lozano.fernandez@gmail.com>
2025-04-05 19:12:50 +0200

    Initial Commit

---
+++ RickAndMortyiOSApp/RickAndMortyiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyiOSAppApp.swift
+//  RickAndMortyiOSApp
+//
+//  Created by Toni Lozano Fernández on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
